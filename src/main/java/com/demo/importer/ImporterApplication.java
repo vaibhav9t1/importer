6f6dbd275216cce156d8c8a416796b739d00350c
vaibhav <vaibhav.waghmare@techprescient.com>
2024-05-27 12:31:26 +0530

    Added importer application code

---
+++ src/main/java/com/demo/importer/ImporterApplication.java
@@ -0,0 +1,15 @@
+package com.demo.importer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+public class ImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImporterApplication.class, args);
+	}
+
+}
